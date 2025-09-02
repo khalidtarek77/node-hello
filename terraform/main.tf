92a73652e6a8dd19a9dfff3f9ad95c43519a9df5
Khalid <khalidtico19@gmail.com>
2025-09-02 20:09:08 +0300

    disregarding new relic

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,16 +4,13 @@ provider "docker" {
 
 # Pull image from GitHub Container Registry (GHCR)
 resource "docker_image" "app" {
-  name = "ghcr.io/khalidtarek77/node-hello:38e9c7a65b11de20a061b0788825d832f758c63f"
+  name = "ghcr.io/khalidtarek77/node-hello:216e55fffa16538415ffad1a8108b9035e717bc0"
 }
 
 # Run container from pulled image
 resource "docker_container" "app" {
   name  = "node-hello-app"
   image = docker_image.app.image_id
-  env = [
-    "NEW_RELIC_LICENSE_KEY=eu01xxc2c40de70a6c66f92ca82aeb38FFFFNRAL"
-  ]
   ports {
     internal = 3000   # app port inside container
     external = 3000   # exposed port on your machine
