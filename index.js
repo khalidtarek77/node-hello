216e55fffa16538415ffad1a8108b9035e717bc0
khalidtarek77 <khalidtico19@gmail.com>
2025-09-02 20:04:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-require('newrelic');  // <-- add this line first
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('App running on port 3000'));
-
 const http = require('http');
 const port = process.env.PORT || 3000;
 
